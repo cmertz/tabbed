00464dad29e586bedcc03c8abd744268ab562ae5
Christoph Mertz <cmertz@users.noreply.github.com>
2020-05-22 17:32:33 +0200

    add custom config

---
+++ config.h
@@ -0,0 +1,43 @@
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+static const char font[]        = "monospace:size=9";
+static const char* normbgcolor  = "#222222";
+static const char* normfgcolor  = "#cccccc";
+static const char* selbgcolor   = "#555555";
+static const char* selfgcolor   = "#ffffff";
+static const char* urgbgcolor   = "#111111";
+static const char* urgfgcolor   = "#cc0000";
+static const char before[]      = "<";
+static const char after[]       = ">";
+static const char titletrim[]   = "...";
+static const int  tabwidth      = 200;
+static const Bool foreground    = True;
+static       Bool urgentswitch  = False;
+
+/*
+ * Where to place a new tab when it is opened. When npisrelative is True,
+ * then the current position is changed + newposition. If npisrelative
+ * is False, then newposition is an absolute position.
+ */
+static int  newposition   = 0;
+static Bool npisrelative  = False;
+
+#define SETPROP(p) { \
+        .v = (char *[]){ "/bin/sh", "-c", \
+                "prop=\"`xwininfo -children -id $1 | grep '^     0x' |" \
+                "sed -e's@^ *\\(0x[0-9a-f]*\\) \"\\([^\"]*\\)\".*@\\1 \\2@' |" \
+                "xargs -0 printf %b | dmenu -l 10 -w $1`\" &&" \
+                "xprop -id $1 -f $0 8s -set $0 \"$prop\"", \
+                p, winid, NULL \
+        } \
+}
+
+#define MODKEY ControlMask
+static Key keys[] = {
+	/* modifier             key        function     argument */
+	{ MODKEY|ShiftMask,     XK_Tab,    rotate,      { .i = -1 } },
+	{ MODKEY,               XK_Tab,    rotate,      { .i = +1 } },
+	{ MODKEY,               XK_q,      killclient,  { 0 } },
+	{ MODKEY,               XK_f,      fullscreen,  { 0 } },
+};
